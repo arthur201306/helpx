abec941e64372977d325823878468b05f6655618
arthur201306 <arthurpaivamuniz@gmail.com>
2023-12-24 21:48:54 -0300

    configurated connection with database

---
+++ backend/database/connection.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"context"
+	"time"
+
+	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+var Connect *pgx.ConnPool
+
+func NewConnection(connectionString string) (*pgxpool.Pool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	conn, err := pgxpool.Connect(ctx, connectionString)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close()
+
+	return conn, nil
+}
